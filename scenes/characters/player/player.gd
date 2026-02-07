dddfac6859df1c82ac18e8327414f8b89aaea1dc
Wesley <yinnihisoka@gmail.com>
2026-02-07 16:45:29 -0300

    feat: adiciona sistema de UI e tool manager
    - Adiciona assets de UI básica
    - Implementa tool_manager para gerenciar ferramentas do jogador
    - Cria cena de teste para game screen UI e navegação NPC
    - Atualiza cenas de personagens (player, chicken, cow)
    - Adiciona componentes de UI (game screen, tools panel)
    - Atualiza configurações do projeto e tileset

--- scenes/characters/player/player.gd
+++ scenes/characters/player/player.gd
@@ -3,8 +3,17 @@
 class_name Player
 extends CharacterBody2D
 
+@onready var hit_component: HitComponent = $HitComponent
+
 ## Armazena a direção que o jogador está olhando (UP, DOWN, LEFT, RIGHT)
 var player_direction: Vector2
 
 ## Ferramenta atualmente equipada pelo jogador
 @export var current_tool: DataTypes.Tools = DataTypes.Tools.None
+
+func _ready() -> void:
+	ToolManager.tool_selected.connect(on_tool_selected)
+	
+func on_tool_selected(tool :DataTypes.Tools)-> void:
+	current_tool = tool
+	hit_component.current_tool = tool
