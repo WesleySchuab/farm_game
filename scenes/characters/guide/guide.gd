344c84647f3e3549ccb34511033a759df12c796a
Wesley <yinnihisoka@gmail.com>
2026-04-19 13:48:16 -0300

    feat: atualizar diálogo e tema
    - Atualizar cena do balão de diálogo
    - Melhorar validação de carregamento no guide.gd
    - Adicionar tema do balão de diálogo

--- scenes/characters/guide/guide.gd
+++ scenes/characters/guide/guide.gd
@@ -2,7 +2,7 @@ extends Node2D
 var balloon_scene  = preload("res://dialogue/game_dialogue_balloon.tscn")
 @onready var interactable_component: InteractableComponents = $InteractableComponent
 @onready var interactable_label_component: Control = $InteractableComponent/InteractableLabelComponent
-
+var in_range: bool
 func _ready() -> void:
 	interactable_component.interactable_activated.connect(on_interectable_activated) 
 	interactable_component.interactable_deactivated.connect(on_interectable_deactivated)
@@ -10,16 +10,19 @@ func _ready() -> void:
 	
 func on_interectable_activated () -> void:
 	interactable_label_component.show()
+	in_range = true
 	
 func on_interectable_deactivated () -> void:
 	interactable_label_component.hide()
+	in_range = false
 	
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("show_dialogue"):
-		var dialogue_resource = load("res://dialogue/conversations/guide.dialogue")
-		if dialogue_resource == null:
-			printerr("❌ Erro: Não foi possível carregar o arquivo de diálogo. Verifique se guide.dialogue existe e está compilado corretamente.")
-			return
-		var balloon: BaseGameDialogueBalloon = balloon_scene.instantiate()
-		get_tree().current_scene.add_child(balloon)
-		balloon.start(dialogue_resource, "start")
+	if in_range:
+		if event.is_action_pressed("show_dialogue"):
+			var dialogue_resource = load("res://dialogue/conversations/guide.dialogue")
+			if dialogue_resource == null:
+				printerr("❌ Erro: Não foi possível carregar o arquivo de diálogo. Verifique se guide.dialogue existe e está compilado corretamente.")
+				return
+			var balloon: BaseGameDialogueBalloon = balloon_scene.instantiate()
+			get_tree().current_scene.add_child(balloon)
+			balloon.start(dialogue_resource, "start")
