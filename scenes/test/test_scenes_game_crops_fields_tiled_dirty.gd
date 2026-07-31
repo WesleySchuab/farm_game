2ba7a04f654260ed6bc3efb594316b2fe9e1f9ef
Wesley <yinnihisoka@gmail.com>
2026-07-31 09:30:30 -0300

    Criando o necromante

--- scenes/test/test_scenes_game_crops_fields_tiled_dirty.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-func _ready() -> void:
-	ToolManager.enable_tool_button(DataTypes.Tools.TillGround)
-	ToolManager.enable_tool_button(DataTypes.Tools.WaterCrops)
-	ToolManager.enable_tool_button(DataTypes.Tools.PlantCorn)
-	ToolManager.enable_tool_button(DataTypes.Tools.PlantTomato)
